127e4a7e0eb78c77dd5a181f773d70dbd9ae9303
josh rotenberg <joshrotenberg@users.noreply.github.com>
2018-03-13 20:09:45 -0700

    cors

--- src/clj/todo/middleware.clj
+++ src/clj/todo/middleware.clj
@@ -5,11 +5,23 @@
             [immutant.web.middleware :refer [wrap-session]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
 
+(defonce cors-headers
+  {"access-control-allow-headers" "accept, content-type"
+   "access-control-allow-methods" "GET,HEAD,POST,DELETE,PUT"
+   "access-control-allow-origin" "*"})
+
+(defn wrap-cors [handler]
+  (fn [request]
+    (let [response (handler request)
+          current-headers (get response :headers {})]
+      (assoc-in response [:headers] (merge current-headers cors-headers)))))
+
 (defn wrap-base [handler]
   (-> ((:middleware defaults) handler)
       wrap-flash
       (wrap-session {:cookie-attrs {:http-only true}})
       (wrap-defaults
-        (-> site-defaults
-            (assoc-in [:security :anti-forgery] false)
-            (dissoc :session)))))
+       (-> site-defaults
+           (assoc-in [:security :anti-forgery] false)
+           (dissoc :session)))
+      wrap-cors))
